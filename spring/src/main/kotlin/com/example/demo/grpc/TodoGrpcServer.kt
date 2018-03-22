d8aa81dec856481ed25afdbf3498899d4b8f50a5
Tnarita0000 <naritatakuya0000@gmail.com>
2018-03-23 04:45:32 +0900

    change proto package name & add todo grpc server process

--- spring/src/main/kotlin/com/example/demo/grpc/TodoGrpcServer.kt
+++ spring/src/main/kotlin/com/example/demo/grpc/TodoGrpcServer.kt
@@ -6,16 +6,34 @@ import io.grpc.stub.StreamObserver
 
 @GRpcService
 class TodoGrpcServer : TodostoreGrpc.TodostoreImplBase() {
-
   override
   fun listTodos(req: ListTodosRequest, res: StreamObserver<ListTodosResponse>) {
     val todosBuilder = ListTodosResponse.newBuilder()
-    val todoBuilder  = GetTodoResponse.newBuilder()
-    todoBuilder.setId(1)
-    todoBuilder.setContent("Hello gRPC!!!")
+    val todoBuilder1  = GetTodoResponse.newBuilder()
+    val todoBuilder2  = GetTodoResponse.newBuilder()
+    val todoBuilder3  = GetTodoResponse.newBuilder()
+    todoBuilder1.setId(1)
+    todoBuilder1.setContent("Hello gRPC!!!")
+    todoBuilder2.setId(2)
+    todoBuilder2.setContent("Hello Kotlin!!!")
+    todoBuilder3.setId(3)
+    todoBuilder3.setContent("Hello Ruby!!!")
 
-    todosBuilder.setTodo(1, todoBuilder.build())
+    todosBuilder.addTodo(todoBuilder1.build())
+    todosBuilder.addTodo(todoBuilder2.build())
+    todosBuilder.addTodo(todoBuilder3.build())
+
     res.onNext(todosBuilder.build())
     res.onCompleted()
   }
+
+  override
+  fun getTodo(req: GetTodoRequest, res: StreamObserver<GetTodoResponse>) {
+    val todoBuilder  = GetTodoResponse.newBuilder()
+    todoBuilder.setId(1)
+    todoBuilder.setContent("Hello gRPC!!!")
+    res.onNext(todoBuilder.build())
+    res.onCompleted()
+  }
+
 }
