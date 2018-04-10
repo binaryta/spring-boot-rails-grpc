269cbf434f0ae067801c6032df7353549f3f3211
Tnarita0000 <naritatakuya0000@gmail.com>
2018-04-11 02:23:35 +0900

    add react frontend dir

---
+++ rails-server/frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    host: '0.0.0.0',
+    port: '8080',
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js(x)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env', 'react']
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: []
+};
