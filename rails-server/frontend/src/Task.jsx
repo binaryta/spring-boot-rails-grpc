1c99a7c103a2b330d4a76799c674386c4775cfd9
成田拓哉 <narita.takuya@adways.net>
2018-04-13 08:01:57 +0000

    add delete icon to done task element

--- rails-server/frontend/src/Task.jsx
+++ rails-server/frontend/src/Task.jsx
@@ -158,6 +158,7 @@ export class Tasks extends React.Component {
               onDragStart={this.dragStart.bind(this)}
               onDragEnd={this.dragEnd.bind(this, task)}
             >
+              <span className="delete-icon" onClick={this.onDeleteClick.bind(this, task)}></span>
               <p className="task-content">{task.content}</p>
             </div>
           )
